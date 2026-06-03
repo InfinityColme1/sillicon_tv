9213437bfdef2ee160a1ad629337dffab27218fc
MauricioLavady <mauricio.lavady@gmail.com>
2026-06-04 00:13:33 +0200

    Navigation pop fix
    Now gesture to pop is supported

--- lib/features/shows/presentation/pages/show_details.dart
+++ lib/features/shows/presentation/pages/show_details.dart
@@ -17,9 +17,16 @@ class ShowDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      appBar: _buildAppBar(context),
-      body: _buildBody(context),
+    return PopScope(
+      onPopInvokedWithResult: (popped, result) {
+        if (popped) {
+          context.read<ShowBloc>().add(GetPopularShows());
+        }
+      },
+        child: Scaffold(
+          appBar: _buildAppBar(context),
+          body: _buildBody(context),
+        )
     );
   }
 
@@ -28,7 +35,6 @@ class ShowDetails extends StatelessWidget {
       leading: IconButton(
           onPressed: () {
             Navigator.pop(context);
-            context.read<ShowBloc>().add(GetPopularShows());
           },
           icon: Icon(Icons.arrow_back)
       ),
